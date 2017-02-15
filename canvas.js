168a475408db17ccb39bb701317f4f1cd4618bae
fhaque1 <farhan3231@gmail.com>
2017-02-14 23:04:57 -0500

    connect the dots

--- canvas.js
+++ canvas.js
@@ -7,24 +7,30 @@ var addRect = function(e) {
 	ctx.fillRect(e.offsetX,e.offsetY,50,100);
 };
 
-var conCirc = function(e) {
+var constructCirc = function(e) {
 	ctx.beginPath();
     ctx.arc(e.offsetX, e.offsetY, 15, 0, 2*Math.PI);
     ctx.fillStyle = "red";
+	ctx.fill();
     ctx.strokeStyle = 'black';
-    ctx.fill();
-    ctx.stroke();
-    ctx.closePath();
+	ctx.stroke();
 };
 
+var circExist = false;
+var connectCirc = function(e) {
+	ctx.lineTo(e.offsetX,e.offsetY);
+	ctx.stroke();
+	constructCirc(e);
+}
 
 //c.addEventListener("click", addRect);
-c.addEventListener("click", conCirc);
+c.addEventListener("click", connectCirc);
 
 
 var b = document.getElementById("b");
 
 var clear = function(e) {
 	ctx.clearRect(0, 0, c.width, c.height);
+	ctx.beginPath();
 };
 b.addEventListener("click", clear);
